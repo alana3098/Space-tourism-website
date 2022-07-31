a74b23d0754e5a44c812390f50214a907592930c
alana3098 <57573144+alana3098@users.noreply.github.com>
2022-07-31 12:59:53 -0400

    hamburger menu added

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("myMenu").style.width = "200px";
+    document.getElementById("myMenu").style.marginLeft = "0px";
+}
+
+function closeNav() {
+    document.getElementById("myMenu").style.width = "0px";
+    document.getElementById("myMenu").style.marginLeft = "300px";
+}
