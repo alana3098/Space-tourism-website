8900bc6e20dddd7a87fb5412660909d44965f72d
alana3098 <57573144+alana3098@users.noreply.github.com>
2022-08-01 13:35:39 -0400

    comments added and margin left moved on js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function openNav() {
-    document.getElementById("myMenu").style.width = "200px";
-    document.getElementById("myMenu").style.marginLeft = "0px";
+    document.getElementById("myMenu").style.width = "200px";/*to open the menu*/
+    document.getElementById("myMenu").style.marginLeft = "200px";
 }
 
 function closeNav() {
-    document.getElementById("myMenu").style.width = "0px";
-    document.getElementById("myMenu").style.marginLeft = "750px";
+    document.getElementById("myMenu").style.width = "0px";/*remove menu*/
+    document.getElementById("myMenu").style.marginLeft = "750px";/*to make it slide away from the users view*/
 }
